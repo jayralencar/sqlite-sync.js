997ed8902ab89b3a8103b247676078beb767afd0
Jayr Alencar <jayralencarpereira@gmail.com>
2016-05-24 11:37:25 -0300

    Testing transactions

--- test/test.js
+++ test/test.js
@@ -4,7 +4,12 @@ sqlite.connect('test/test.db');
 // sqlite.debug = true;
 
 var res = sqlite.run("CREATE TABLE COMPANYS(ID  INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT NOT NULL);");
-console.log(res)
+
+sqlite.insert("COMPANYS",{NAME:"TESTE"}, function(res){
+
+});
+
+sqlite.run("BEGIN; DELETE FROM COMPANYS WHERE ID = 6; ROLLBACK;");
 
 sqlite.run('SELECT * FROM COMPANYS', function(res){
 	if(res.error)
